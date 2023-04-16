b41d8613709b725fa7b1de972b4603a84333fcbb
Aayush01Mishra <aayushmishra0512@gmail.com>
2023-04-16 12:55:17 +0530

    final1

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import "./Style.css"
 import { Link } from "react-router-dom";
-import ethers from 'ethers';
 export const Login = (props) => {
     const [email, setEmail]= useState('')
     const [pw, setPassword]= useState('')
